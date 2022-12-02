880407cf4880dd8390a5e94d3d40ebb382034742
Lucky-Star-999 <theboost1305@gmail.com>
2022-12-02 09:56:44 +0700

    Add some base files

--- fe-calendar/src/App.js
+++ fe-calendar/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import InvitationPending from "./invitationpending/InvitationPending.js";
+import Homepage from "./homepage/Homepage.js";
+import Invitation from "./invitation/Invitation.js";
+import Login from "./user/Login.js";
+import CreateNewUser from "./user/CreateNewUser.js";
+import CreateUserResult from "./user/CreateUserResult.js";
+import CreateNewEvent from "./createnewevent/CreateNewEvent.js";
+import CreateEventResult from "./createnewevent/CreateEventResult.js";
+import EditEvent from "./editevent/EditEvent.js";
+import Profile from "./profile/Profile.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Login />}></Route>
+					<Route path="/home" element={<Homepage />}></Route>
+					<Route path="/invitations" element={<Invitation />}></Route>
+					<Route path="/pending-invitations" element={<InvitationPending />}></Route>
+					<Route path="/create-new-event" element={<CreateNewEvent />}></Route>
+					<Route path="/create-new-event/result" element={<CreateEventResult />}></Route>
+					<Route path="/edit-event" element={<EditEvent />}></Route>
+					<Route path="/create-user" element={<CreateNewUser />}></Route>
+					<Route path="/create-user/result" element={<CreateUserResult />}></Route>
+					<Route path="/profile" element={<Profile />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
