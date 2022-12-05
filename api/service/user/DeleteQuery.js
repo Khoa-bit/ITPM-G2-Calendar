de5ede5ce323a729a10741ccaa3fdfb46cefac8b
Lucky-Star-999 <theboost1305@gmail.com>
2022-12-05 21:36:55 +0700

    Fix Delete Account

--- api/service/user/DeleteQuery.js
+++ api/service/user/DeleteQuery.js
@@ -1,6 +1,26 @@
 // Delete an user
 exports.deleteUser = (email) => {
-    let query = 
+    let query =
         `DELETE FROM userinformation WHERE email = '${email}';`;
+    return query;
+}
+
+// Delete all invitations related to user (For delete user)
+exports.deleteUser_invitations = (email) => {
+    let query =
+        `DELETE FROM
+            invitation 
+        WHERE
+            eventid IN ( SELECT eventid FROM event WHERE hostemail = '${email}' );`;
+    return query;
+}
+
+// Delete all events related to user (For delete user)
+exports.deleteUser_events = (email) => {
+    let query =
+        `DELETE FROM
+            event 
+        WHERE
+            eventid IN ( SELECT eventid FROM event WHERE hostemail = '${email}' );`;
     return query;
 }
